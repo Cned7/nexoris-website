7851ede29e7280077708612ae75ebc1e96c10b40
Cned7 <a.chinedu07@gmail.com>
2025-04-16 13:28:50 +0100

    removed markdown

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,26 +1,12 @@
-// app/blogs/[slug]/page.tsx.
 'use client'
+
 import { useEffect, useState, use } from 'react'
-import { getPostBySlug } from '../../../lib/api' // Import your API function
+import { getPostBySlug } from '../../../lib/api'
 import { useRouter } from 'next/navigation'
 import { BlogPost } from '@/lib/types'
-import Markdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
-import { FaClipboard } from 'react-icons/fa' // Import your chosen icon
 import moment from 'moment'
-import { toast } from 'react-hot-toast'
 import Image from 'next/image'
-
-const handleCopyCode = async (code: string) => {
-  try {
-    await navigator.clipboard.writeText(code)
-    toast.success('Code copied to clipboard!') // Show toast on error
-  } catch (err) {
-    console.error('Failed to copy code: ', err)
-  }
-}
+import React from 'react'
 
 const BlogPostPage = ({ params }: { params: Promise<{ slug: string }> }) => {
   const { slug } = use(params)
@@ -33,7 +19,6 @@ const BlogPostPage = ({ params }: { params: Promise<{ slug: string }> }) => {
     const fetchPost = async () => {
       if (slug) {
         try {
-          // Fetch the post using the slug
           const fetchedPost = await getPostBySlug(slug)
           setPost(fetchedPost)
         } catch (err) {
@@ -52,19 +37,21 @@ const BlogPostPage = ({ params }: { params: Promise<{ slug: string }> }) => {
     return (
       <div className="max-w-screen-md mx-auto flex items-center justify-center"></div>
     )
+
   if (error) return <p className="max-w-screen-md mx-auto">Error: {error}</p>
+
   if (!post) return <p className="max-w-screen-md mx-auto">No post found.</p>
-  console.log(post)
+
   return (
     <div className="max-w-screen-md mx-auto p-4 mt-28">
       <h1 className="text-4xl leading-[60px] capitalize text-center font-bold text-heading font-jet-brains">
         {post.title}
       </h1>
+
       <div className="w-full flex items-center justify-center font-light">
         Published: {moment(post.createdAt).fromNow()}
       </div>
 
-      {/* Categories Section */}
       {post.categories && post.categories.length > 0 && (
         <div className="flex flex-wrap space-x-2 my-4">
           {post.categories.map(({ name, documentId }) => (
@@ -84,46 +71,18 @@ const BlogPostPage = ({ params }: { params: Promise<{ slug: string }> }) => {
             src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${post.cover.url}`}
             alt={post.title}
             className="rounded-lg w-full h-full object-cover"
+            fill
           />
         </div>
       )}
-      <div className={'leading-[40px] max-w-screen-lg prose prose-invert'}>
-        <Markdown
-          remarkPlugins={[remarkGfm]}
-          components={{
-            code({ inline, className, children, ...props }: any) {
-              const match = /language-(\w+)/.exec(className || '')
-              const codeString = String(children).replace(/\n$/, '')
 
-              return !inline && match ? (
-                <div className="relative">
-                  <button
-                    onClick={() => handleCopyCode(codeString)}
-                    className="absolute top-2 right-2 bg-gray-700 text-white p-1 rounded-md hover:bg-gray-600"
-                    title="Copy to clipboard"
-                  >
-                    <FaClipboard color="#fff" />
-                  </button>
-                  <SyntaxHighlighter
-                    style={dracula}
-                    PreTag="div"
-                    language={match[1]}
-                    {...props}
-                  >
-                    {codeString}
-                  </SyntaxHighlighter>
-                </div>
-              ) : (
-                <code className={className} {...props}>
-                  {children}
-                </code>
-              )
-            },
-          }}
-        >
-          {post.content}
-        </Markdown>
+      <div className="prose prose-invert max-w-screen-lg leading-[40px]">
+        <div
+          dangerouslySetInnerHTML={{ __html: post.content }}
+          className="ck-content"
+        />
       </div>
+
       <button
         onClick={() => router.back()}
         className="text-purple-800 mt-4 inline-block hover:underline"
